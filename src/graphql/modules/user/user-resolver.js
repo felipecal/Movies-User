5db9537a020d37f943c3f3e3a7f695e339f512de
Felipe <fcalldas@gmail.com>
2022-06-25 11:20:49 -0300

    ✨ introducing new features

--- src/graphql/modules/user/user-resolver.js
+++ src/graphql/modules/user/user-resolver.js
@@ -2,8 +2,42 @@ import sequelize from 'sequelize';
 
 export default {
     Query: {
-        getAllUser: async (parent, _, {database}, info) => {
+        getUserById: async (parent, { id }, { database }, info) => {
+            return await database.Users.findByPk(id)
+        },
+        getAllUser: async (parent, _, { database }, info) => {
             return await database.Users.findAll()
+        }
+    },
+    Mutation: {
+        createUser: async (parent, { input }, { database }, info) => {
+            return await database.Users.create({
+                firstName: input.firstName,
+                lastName: input.lastName,
+                email: input.email
+            })
+        },
+        updateUser: async (parent, { id, input }, { database }, info) => {
+            const user = await database.Users.findByPk(id);
+            if (!user) {
+                throw new Error('User not found');
+            } else {
+                const updateUser = await user.update(input);
+                return updateUser;
+            }
+        },
+        deleteUser: async (parent, { id }, { database }, info) => {
+            const user = await database.Users.findByPk(id);
+            if (!user){
+                throw new Error ('User not found');
+            }else {
+                const deleteUser = await user.update({
+                    removed_at: new Date()
+                }); 
+                return !!deleteUser;
+            }
+
+
         }
     }
 }
